537aa1d8cf68c894e7c4a178d85a0d6d0b575a84
Pandapoc <45634681+Pandapoc@users.noreply.github.com>
2019-05-21 19:45:41 -0700

    cleared sql path

--- config/index.js
+++ config/index.js
@@ -5,7 +5,7 @@ if (process.env.NODE_ENV === 'production') {
 
   sequelize = new Sequelize(process.env.JAWSDB_URL)
 } else {
-  sequelize = new Sequelize('musql://root:Nickisawesome1!@localhost:3306/todo_db')
+  sequelize = new Sequelize('your path here')
 }
 
 module.exports = sequelize
