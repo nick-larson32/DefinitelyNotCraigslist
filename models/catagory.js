04cca32cffefe36dcb76721beff22ea4e9bc0fab
Pandapoc <45634681+Pandapoc@users.noreply.github.com>
2019-05-23 18:46:27 -0700

    added catagories

--- models/catagory.js
+++ models/catagory.js
@@ -1,7 +1,13 @@
 module.exports = (sequelize, Sequelize) => {
   class Catagory extends Sequelize.Model {}
   Catagory.init({
-    Catagory: { type: Sequelize.STRING, allowNull: false }
+    Catagory: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      isIn: [
+        ['Furniture', 'Kitchen Ware', 'Electronics', 'Cloths', 'Food', 'Toys', 'Misc.']
+      ]
+    }
   }, {
     sequelize,
     modelName: 'catagory'
