9146ce75c2d5231ed73b08b2b5b6eb56cc540e2b
C.Nguyen <cdkedits@gmail.com>
2019-05-23 18:48:37 -0700

    added yelp API code

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -44,3 +44,20 @@
   };
 
 }(this, this.document));
+
+// Yelp API code
+let URL
+
+const API_KEY = 'M7IgpDGg-9kXeDXpl5Yj9A9_33reRjvfXANnx3RvJzphIo_pAXUEvP5FubmdzBV32ehGAyXtlzo0_hjlxzrBPi4O705EnwbTMXBu1v3rbP78tARuwNYSElA1WBqjXHYx';
+
+let queryURL = `https://cors-anywhere.herokuapp.com/${URL}`;
+
+let yelpObject = {
+  method: "GET",
+  headers: {
+    "accept": "application/json",
+    "x-requested-with": "xmlhttprequest",
+    "Access-Control-Allow-Origin": "*",
+    "Authorization": `Bearer ${API_KEY}`
+  }
+}
