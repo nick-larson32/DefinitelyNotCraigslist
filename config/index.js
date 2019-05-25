15f4ad36375bc16f2e85e5e37a8210fd9cc68d59
SPFlores <sflores92886@gmail.com>
2019-05-25 12:24:07 -0700

    small edits, API key

--- config/index.js
+++ config/index.js
@@ -1,7 +1,6 @@
 const Sequelize = require('sequelize')
 require('dotenv').config()
 let sequelize
-require('dotenv').config()
 
 if (process.env.NODE_ENV === 'production') {
   sequelize = new Sequelize(process.env.JAWSDB_URL)
--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,7 +1,9 @@
+require('dotenv').config()
+
 // Yelp API code
 let URL
 
-const API_KEY = 'M7IgpDGg-9kXeDXpl5Yj9A9_33reRjvfXANnx3RvJzphIo_pAXUEvP5FubmdzBV32ehGAyXtlzo0_hjlxzrBPi4O705EnwbTMXBu1v3rbP78tARuwNYSElA1WBqjXHYx';
+const API_KEY = process.env.API_KEY;
 
 let queryURL = `https://cors-anywhere.herokuapp.com/${URL}`;
 
