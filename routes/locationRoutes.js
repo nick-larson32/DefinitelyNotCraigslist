c1e0b202a2a2bd2f472dbfcf7bbee7ad3e13af43
Pandapoc <45634681+Pandapoc@users.noreply.github.com>
2019-05-25 10:24:10 -0700

    Location routes updated

--- routes/locationRoutes.js
+++ routes/locationRoutes.js
@@ -10,29 +10,8 @@ module.exports = app => {
 
   // GET one
   app.get('/locations/:id', (req, res) => {
-    Location.findOne({ where: { id: req.params.id } })
+    Location.findOne({ where: { id: req.params.id }, include: [{ model: User, include: [{ model: Item, include: [Category] }] }] })
       .then(location => res.json(location))
-      .catch(e => console.log(e))
-  })
-
-  // POST one
-  app.post('/locations', (req, res) => {
-    Location.create(req.body)
-      .then(_ => res.sendStatus(200))
-      .catch(e => console.log(e))
-  })
-
-  // PUT one
-  app.put('/locations/:id', (req, res) => {
-    Location.update(req.body, { where: { id: req.params.id } })
-      .then(_ => res.sendStatus(200))
-      .catch(e => console.log(e))
-  })
-
-  // DELETE one
-  app.delete('/locations/:id', (req, res) => {
-    Location.destroy({ where: { id: req.params.id } })
-      .then(_ => res.sendStatus(200))
       .catch(e => console.log(e))
   })
 }
