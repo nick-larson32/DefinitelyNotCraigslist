19824a64cfc0dd1d18ec5d3eea55bfb8f0a8afc5
SPFlores <sflores92886@gmail.com>
2019-05-27 15:55:34 -0700

    get user's items

--- public/assets/js/getUsers.js
+++ public/assets/js/getUsers.js
@@ -3,22 +3,45 @@ const { fetch } = window
 document.addEventListener('click', e => {
   if (e.target.id === 'users') {
     users()
+  } else if (e.target.id === 'getUserItems') {
+    // console.log('this works')
+    users()
   }
 })
 
+// this needs to change to be reflective of the current user logged in
+// for now, hardcoded as 1 for demo purposes
+let userId = 1
+
 const users = _ => {
-  fetch('/users')
+  fetch(`/users/${userId}`)
     .then(r => r.json())
-    .then(users => users.map(user => {
-      if (user.id === 1) {
-        document.querySelector('.userDiv').innerHTML = ''
-        let userDiv = document.createElement('div')
-        userDiv.innerHTML = `
+    .then(user => {
+      document.querySelector('.user').innerHTML = ''
+      let userDiv = document.createElement('div')
+      userDiv.innerHTML = `
         <h1>${user.name}</h1>
         <h3>${user.address}</h3>
         `
-        document.querySelector('.userDiv').append(userDiv)
-      }
-    }))
+      document.querySelector('.user').append(userDiv)
+
+      document.querySelector('.userItems').innerHTML = ''
+      let itemLocation = user.location.county
+      user.items.forEach(({ itemName, quantity, available, bought, price, condition, description, category }) => {
+        let itemDiv = document.createElement('div')
+        itemDiv.innerHTML = `
+          <h1>Item Name: ${itemName}</h1>
+          <h3>Quantity: ${quantity}</h3>
+          <h4>Available (donator):${available}</h4>
+          <h4>Bought (buyer):${bought}</h4>
+          <h3>Price: $${price}</h3>
+          <h4>Condition: ${condition}</h4>
+          <h5>Description: ${description}</h5>
+          <h5>Category it belongs to: ${category.category}</h5>
+          <h5>Location: ${itemLocation}</h5>
+          `
+        document.querySelector('.userItems').append(itemDiv)
+      })
+    })
     .catch(e => console.log(e))
 }
