18de790b22212a864948d660e45c97c4f374eb5c
YourName <safavis@uci.edu>
2019-05-30 21:28:57 -0700

    adding my changes to my branch

---
+++ public/assets/js/browseapp.js
@@ -0,0 +1,5 @@
+let craiglistLogin=localStorage.getItem('CraiglistLogin')
+console.log(craiglistLogin.json())
+ if(craiglistLogin.logedin)
+ {console.log(craiglistLogin.logedin)
+   document.querySelector('.sectionMain').innerHTML='<i class="material-icons tiny iconStyle">ac_unit</i>'+document.querySelector('.sectionMain').innerHTML}
