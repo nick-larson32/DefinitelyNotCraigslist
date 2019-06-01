23495e41c62c969ad27371c001015d996baeb3af
C.Nguyen <cdkedits@gmail.com>
2019-05-31 18:06:11 -0700

    added tiles.js

---
+++ public/assets/js/tiles.js
@@ -0,0 +1,22 @@
+// Code to make tiles have responsive shadows when touched
+const getParent = event => {
+  return event.target.closest(`a`)
+}
+
+document.addEventListener(`touchstart`, event => {
+  let parent = getParent(event)
+  if (event.target.classList[0] === `hover`) {
+    let parentLink = document.querySelector(`a[data-id="${event.target.dataset.id}"]`)
+    parentLink.classList.add(`hover-active`)
+    console.log(document.querySelector(`a[data-id="${event.target.dataset.id}"]`).classList)
+  }
+})
+
+document.addEventListener(`touchend`, event => {
+  let parent = getParent(event)
+  if (event.target.classList[0] === `hover`) {
+    let parentLink = document.querySelector(`a[data-id="${event.target.dataset.id}"]`)
+    document.querySelector(`a[data-id="${event.target.dataset.id}"]`).classList.remove(`hover-active`)
+    console.log(document.querySelector(`a[data-id="${event.target.dataset.id}"]`).classList)
+  }
+})
