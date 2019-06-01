8c47399ecb93726c57fbea0559960b47d1404a04
C.Nguyen <cdkedits@gmail.com>
2019-06-01 13:37:45 -0700

    added js to populate clothes.html

--- public/assets/js/category.js/clothes.js
+++ public/assets/js/category.js/clothes.js
@@ -1,14 +1,54 @@
 const { fetch } = window
 
-const getItems = _ => {
-  fetch(`/items`)
-    .then(r => r.json())
+// const getItems = _ => {
+//   fetch(`/items`)
+//     .then(r => r.json())
+//     .then(items => {
+//       items.forEach(({itenName, quantity }) => {
+//         console.log(itemName, quantity)
+//       })
+//     })
+//     .catch(e => console.log(e))
+// }
+
+const getAllItems = _ => {
+  fetch('/items')
+    .then(items => items.json())
     .then(items => {
-      items.forEach(({itenName, quantity }) => {
-        console.log(itemName, quantity)
+      document.querySelector('#clothesTiles').innerHTML = ''
+      items.forEach(({ itemName, quantity, available, bought, price, condition, description, user, category }) => {
+        let clothesDiv = document.createElement('div')
+        if (available && !bought && quantity > 0) {
+          clothesDiv.innerHTML = `
+            <div class="card">
+              <div class="card-content">
+                <p class="title">
+                  ${itemName}
+                </p>
+                <p class="subtitle">
+                  Number available: ${quantity}
+                  Category: ${category.category}
+                </p>
+              </div>
+              <footer class="card-footer">
+                <p class="card-footer-item">
+                  <span>
+                  View on <a href="https://twitter.com/codinghorror/status/506010907021828096">Twitter</a>
+                  </span>
+                </p>
+                <p class="card-footer-item">
+                  <span>
+                  Share on <a href="#">Facebook</a>
+                  </span>
+                </p>
+              </footer>
+            </div>
+          `
+          document.querySelector('#clothesTiles').append(clothesDiv)
+        }
       })
     })
-    .catch(e => console.log(e))
+    .catch(e => console.error(e))
 }
 
-getItems()
+getAllItems()
