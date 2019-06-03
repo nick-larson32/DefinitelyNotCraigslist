a2dbb5fd53afb7df82b86ede6deeb1c8bff1de17
SPFlores <sflores92886@gmail.com>
2019-06-02 20:48:39 -0700

    login, logout working

--- public/assets/js/browseapp.js
+++ public/assets/js/browseapp.js
@@ -1,16 +1,12 @@
 let loggedInUser = sessionStorage.getItem('id')
 
-
 if (loggedInUser) {
   // make div/bar across visbile
   document.querySelector('#userLoggedIn').style.display = 'block'
-  // hello name dynamic
   document.querySelector('#hello').innerHTML = `
   Hello ${sessionStorage.getItem('name')}!
   `
-  // logout
-    // clear session storage
-    // take to browse page
-} else {
-  // make the div go hidden
+} else if (loggedInUser === '') {
+  // display log in link
+  document.querySelector('#userLogIn').style.display = 'block'
 }
