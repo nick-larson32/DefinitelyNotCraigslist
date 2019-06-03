56f39e4525547da29e951ca2fbfe240af04019d7
SPFlores <sflores92886@gmail.com>
2019-06-02 20:23:51 -0700

    logout working, displaying menu for user

--- public/assets/js/browseapp.js
+++ public/assets/js/browseapp.js
@@ -1,36 +1,16 @@
-let craiglistLogin = JSON.parse(localStorage.getItem('CraiglistLogin'))
-console.log(craiglistLogin)
-console.log(localStorage.getItem('CraiglistLogin'))
-if (craiglistLogin.logedin) {
-  console.log(craiglistLogin.logedin)
-  document.querySelector('.sectionMain').innerHTML =
-    `
-   <nav class="navbar" role="navigation" aria-label="main navigation">
-  <div class="navbar-brand">
-  <i class="material-icons medium" style="display:inline;">account_box</i>
-  <div class="navbar-item has-dropdown is-hoverable">
-  <a class="navbar-link">
-    More
-  </a>
+let loggedInUser = sessionStorage.getItem('id')
 
-  <div class="navbar-dropdown">
-    <a class="navbar-item">
-      About
-    </a>
-    <a class="navbar-item">
-      Jobs
-    </a>
-    <a class="navbar-item">
-      Contact
-    </a>
-    <hr class="navbar-divider">
-    <a class="navbar-item">
-      Report an issue
-    </a>
-  </div>
-  </div>
-  </nav>
-   `
-    + document.querySelector('.sectionMain').innerHTML
+
+if (loggedInUser) {
+  // make div/bar across visbile
+  document.querySelector('#userLoggedIn').style.display = 'block'
+  // hello name dynamic
+  document.querySelector('#hello').innerHTML = `
+  Hello ${sessionStorage.getItem('name')}!
+  `
+  // logout
+    // clear session storage
+    // take to browse page
+} else {
+  // make the div go hidden
 }
-//    `<i class="material-icons tiny iconStyle" style="margin:5%;">ac_unit</i><p>Welcome ${craiglistLogin.name}</p>`+document.querySelector('.sectionMain').innerHTML}
