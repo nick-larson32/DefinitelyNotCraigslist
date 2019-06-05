2bc5d8fc3adb48adc9aa7d6f91d4cf6d801c7f92
SPFlores <sflores92886@gmail.com>
2019-06-04 20:57:55 -0700

    cleaning up files, signup

--- public/assets/js/loginoutbar.js
+++ public/assets/js/loginoutbar.js
@@ -11,3 +11,12 @@ if (loggedInUser) {
   document.querySelector('#userSigningUp').style.display = 'inline-block'
   document.querySelector('#logout').style.display = 'none'
 }
+
+document.addEventListener('click', e => {
+  if (e.target.id === 'logout') {
+    sessionStorage.setItem('id', '')
+    sessionStorage.setItem('address', '')
+    sessionStorage.setItem('name', '')
+    location.reload()
+  }
+})
