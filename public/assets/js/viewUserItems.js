76ad3799da2e6e72bde68e7529690e0287b6a053
dien <iamdiennn@gmail.com>
2019-06-05 21:57:07 -0700

    css changes

--- public/assets/js/viewUserItems.js
+++ public/assets/js/viewUserItems.js
@@ -8,35 +8,35 @@ const getUserItems = _ => {
   fetch(`users/${id}`)
     .then(r => r.json())
     .then(user => {
-      user.items.forEach(({ id, itemName, category, condition, price, quantity, bought, available }) => {
+      user.items.forEach(({ id, itemName, category, condition, price, quantity, bought, available, description }) => {
         let userItemDiv = document.createElement('div')
         userItemDiv.innerHTML = `
                <div class="card">
-               <header class="card-harder">
-                      <p class="card-header-title"> ${itemName}</p>
+               <header class="card-harder itemHeader">
+                      <p class="card-header-title itemHeader"> ${itemName}</p>
                </header>
                <div class="card-content">
                <div class="media">
-               <div class="media-left">
-                  <figure class="image is-128x128">
+                  <figure class="image is-128x128 align="middle">
                       <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image">
                   </figure>
-               </div>
                <div class="media-content">
                </div>
               </div>
               <div class="content">
+                <p data-itemid=${id}>Category: ${category.category}</p>
                   <p data-itemid=${id}>Quantity: ${quantity}</p>
                   <p data-itemid=${id}>Available:${available}</p>
-                  <p data-itemid=${id}>Price: $${price}</p>
+                  <p data-itemid=${id}>$${price}</p>
                   <p data-itemid=${id}>Condition: ${condition}</p>
+                  <p data-itemid=${id}>${description}</p>
                   <p data-itemid=${id}>Category: ${category.category}</p>
                   <p data-itemid=${id}>Location: ${user.location.county}</p>
               </div>
                </div>
                <footer class="card-footer">
-                   <a class="card-footer-item itemEdit" data-upid=${id}>Edit</a>
-                   <a class="card-footer-item itemDel" data-id=${id}>Delete</a>
+                   <a class="card-footer-item itemEdit" data-upid=${id}> <i class="fas fa-edit"></i></a>
+                   <a class="card-footer-item itemDel" data-id=${id}><i class="fa fa-trash"></i></a>
                    <a class="card-footer-item soldButton" data-available="${available}">Mark Sold</a>
                </footer>
            </div>
